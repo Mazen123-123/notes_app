4b5b1d40f7442425e1ef4dcec1a356feae185d4a
Mazen123-123 <mazentayal2@gmail.com>
2024-09-18 18:55:18 +0300

    create Notes List View

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -3,27 +3,26 @@ import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'custom_app_bar.dart';
 import 'custom_note_item.dart';
+import 'note_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: const Column(
-        children: [
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: const [
           SizedBox(
             height: 35,
           ),
           CustomAppBar(),
-          SizedBox(
-            height: 10,
+          Expanded(
+            child: NotesListView(),
           ),
-          NoteItem(),
         ],
       ),
     );
   }
 }
-
