a84f60b40eed92b62596ebd2f3ad6f03120e9f22
Mazen123-123 <mazentayal2@gmail.com>
2024-09-26 09:41:50 +0300

    dispaly notes part one

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'notes_state.dart';
@@ -8,13 +9,11 @@ part 'notes_state.dart';
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
 
-  fetchAllNotes() async {
-    try {
-      var notesBox = Hive.box<NoteModel>('notes');
-     List<NoteModel> notes  = notesBox.values.toList();
-      emit(NotesSuccess(notes));
-    } catch (e) {
-      emit(NotesFaliure(e.toString()));
-    }
+  List<NoteModel>? notes;
+
+  fetchAllNotes() {
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
+     notes = notesBox.values.toList();
+    
   }
 }
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 
 import 'custom_app_bar.dart';
 
 import 'note_list_view.dart';
 
-class NotesViewBody extends StatelessWidget {
+class NotesViewBody extends StatefulWidget {
   const NotesViewBody({super.key});
+
+  @override
+  State<NotesViewBody> createState() => _NotesViewBodyState();
+}
+
+class _NotesViewBodyState extends State<NotesViewBody> {
+  @override
+  void initState() {
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes(); 
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +32,7 @@ class NotesViewBody extends StatelessWidget {
           ),
           CustomAppBar(
             title: 'Notes',
-            icon: Icons.search ,
+            icon: Icons.search,
           ),
           Expanded(
             child: NotesListView(),
